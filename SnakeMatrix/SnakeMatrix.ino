7a8ec8041834504bfdbbf9a8a855d8806f038451
daformat <mat.jouhet@gmail.com>
2013-08-19 09:25:51 +0200

    Remove PS3 controller, using pushbuttons instead

--- SnakeMatrix/SnakeMatrix.ino
+++ SnakeMatrix/SnakeMatrix.ino
@@ -1,45 +1,81 @@
-#include "SPI.h"
-#include "Adafruit_WS2801.h"
+/***************************************************
+  Arduino SNAKE
+  -------------
+  forked from:
+  https://github.com/TKJElectronics/LEDStringMatrix
+ ****************************************************/
 
-#include <PS3BT.h>
-USB Usb;
-BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
-/* You can create the instance of the class in two ways */
-PS3BT PS3(&Btd); // This will just create the instance
-//PS3BT PS3(&Btd,0x00,0x15,0x83,0x3D,0x0A,0x57); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
+// You can use any (4 or) 5 pins
+#define sclk 13
+#define mosi 11
+#define cs   10
+#define dc   8
+#define rst  0  // you can also connect this to the Arduino reset
 
-int dataPin  = 2;    // Yellow wire on Adafruit Pixels
-int clockPin = 3;    // Green wire on Adafruit Pixels
+// WS2801
+uint8_t dataPin  = 2;    
+uint8_t clockPin = 3;
 
-// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
-Adafruit_WS2801 strip = Adafruit_WS2801(50, dataPin, clockPin);
+// push buttons
+uint8_t upBtnPin = 4;
+uint8_t dwnBtnPin = 5;
+uint8_t lftBtnPin = 6;
+uint8_t rgtBtnPin = 7;
+
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+
+#include <SPI.h>
+#include <Adafruit_WS2801.h>
+
+// Option 1: use any pins but a little slower
+//Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
+
+// Option 2: must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+
+Adafruit_WS2801 strip = Adafruit_WS2801((uint16_t)7, (uint16_t)14, dataPin, clockPin);
+
+
+#define BUTTON_NONE 0
+#define BUTTON_DOWN 1
+#define BUTTON_RIGHT 2
+#define BUTTON_SELECT 3
+#define BUTTON_UP 4
+#define BUTTON_LEFT 5
+
+#define USE_ST7735 false
 
 
 byte oldSnakeItems = 0;
-byte oldSnakeItemPosX[50];
-byte oldSnakeItemPosY[50];
+byte oldSnakeItemPosX[98];
+byte oldSnakeItemPosY[98];
 byte SnakeItems = 0;
-byte SnakeItemPosX[50];
-byte SnakeItemPosY[50];
+byte SnakeItemPosX[98];
+byte SnakeItemPosY[98];
 byte ApplePosX;
 byte ApplePosY;
 byte AppleMoveCountDown;
-#define APPLE_COUNTDOWN_STEPS_MIN 3
-#define APPLE_COUNTDOWN_STEPS_MAX 7
+#define APPLE_COUNTDOWN_STEPS_MIN 8
+#define APPLE_COUNTDOWN_STEPS_MAX 15
 
-#define BaneGridXmax 7 // Bredden på banen
-#define BaneGridYmax 7 // Højden på banen
+#define BaneGridXmax 7 // 
+#define BaneGridYmax 14 // 
 #define BLANK 0
 #define SNAKE 1
 #define APPLE 2
 byte Playfield[BaneGridXmax+1][BaneGridYmax+1];
 
-	
-byte SnakeHeadID = 0; // Array ID på forreste Snake item (Hoved)
-byte SnakeBackID = 0; // Array ID på bagerste Snake item
+
+byte SnakeHeadID = 0; // Array ID of the head
+byte SnakeBackID = 0; // Array ID of the tail
 
 byte AppleCount = 0;
-	
+
 #define SNAKE_LEFT 0
 #define SNAKE_RIGHT 1
 #define SNAKE_UP 2
@@ -53,59 +89,150 @@ byte SnakeItemsToAddAtApple = 1; // Hvor mange Snake objekter der skal tilføjes
 byte Score = 0;
 boolean GameRunning = false;
 
-void setup()
-{  
-  Serial.begin(9600);
-    
-  strip.begin();
+long timeBefore = millis();
 
-  // Update LED contents, to start they are all 'off'
-  strip.show();  
-  
-  if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while(1); //halt
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("Setup ");
+
+  if (USE_ST7735) {
+    Serial.println("using Adafruit ST7735 TFT screen");
+
+    setupST7735();
   }
-  Serial.print(F("\r\nPS3 Bluetooth Library Started"));     
+  else {
+    Serial.println("using LEDs");
+
+     setupLED();
+  }
 }
 
-long timeBefore = millis();
-void loop()
-{
-  Usb.Task();
-       
-  if (PS3.PS3Connected || PS3.PS3NavigationConnected) { 
-    if (PS3.buttonPressed && PS3.getButton(PS)) 
-     NewGame();
-    
-    if ((millis() >= (timeBefore+500)) && (GameRunning == true)) {
+void setupST7735(void){
+  
+  tft.initR(INITR_REDTAB);   // initialize a ST7735R chip, red tab
+  tft.fillScreen(ST7735_BLACK);
+
+}
+
+void setupLED(){
+
+  strip.begin();
+  // Update LED contents, to start they are all 'off'
+  strip.show();  
+  //NewGame();
+}
+
+void loop() {
+  if (USE_ST7735) {
+    loopST7735();
+  }
+  else {
+    loopLED();
+  }
+}
+
+void loopST7735() {
+  
+  tft.invertDisplay(true);
+  delay(1000);
+  tft.invertDisplay(false);
+  delay(1000);
+}
+
+void loopLED() {
+    if ((millis() >= (timeBefore+300)) && (GameRunning == true)) {
       moveSnake();
       timeBefore = millis();
     }
     
-  // Read directional keys here  
-    if ((PS3.getAnalogHat(LeftHatX) > 150) && ((PS3.getAnalogHat(LeftHatY) < 150) && (PS3.getAnalogHat(LeftHatY) > 80)) && (movingDirection != LEFT)) // Joystick right
-      snakeDirection = SNAKE_RIGHT;
-    if ((PS3.getAnalogHat(LeftHatX) < 80) && ((PS3.getAnalogHat(LeftHatY) < 150) && (PS3.getAnalogHat(LeftHatY) > 80)) && (movingDirection != RIGHT)) // Joystick left
-      snakeDirection = SNAKE_LEFT;      
-  
-    if ((PS3.getAnalogHat(LeftHatY) > 150) && ((PS3.getAnalogHat(LeftHatX) < 150) && (PS3.getAnalogHat(LeftHatX) > 80)) && (movingDirection != UP)) // Joystick down
+    if(digitalRead(upBtnPin) == LOW){
+      Serial.println("UP");
+      snakeDirection = SNAKE_UP;
+      if(!GameRunning) NewGame();
+    }
+    if(digitalRead(dwnBtnPin) == LOW){
+      Serial.println("DOWN");
       snakeDirection = SNAKE_DOWN;
-    if ((PS3.getAnalogHat(LeftHatY) < 80) && ((PS3.getAnalogHat(LeftHatX) < 150) && (PS3.getAnalogHat(LeftHatX) > 80)) && (movingDirection != DOWN)) // Joystick up
-      snakeDirection = SNAKE_UP;  
-      
-    if (PS3.buttonPressed) {
-      if (PS3.getButton(RIGHT))
-        snakeDirection = SNAKE_RIGHT;
-      if (PS3.getButton(LEFT))
-        snakeDirection = SNAKE_LEFT;
-      
-      if (PS3.getButton(DOWN))
-        snakeDirection = SNAKE_DOWN;
-      if (PS3.getButton(UP))
-        snakeDirection = SNAKE_UP;
-    }        
+      if(!GameRunning) NewGame();
+    }
+    if(digitalRead(lftBtnPin) == LOW){
+      Serial.println("LEFT");
+      snakeDirection = SNAKE_LEFT;
+      if(!GameRunning) NewGame();
+    }
+    if(digitalRead(rgtBtnPin) == LOW){
+      Serial.println("RIGHT");
+      snakeDirection = SNAKE_RIGHT;
+      if(!GameRunning) NewGame();
+    }
+    
+    if(!GameRunning)
+      rainbowCycle(1);    
+    
+}
+
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  boolean play = true;
+
+  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
+      if(digitalRead(upBtnPin) == LOW || digitalRead(dwnBtnPin) == LOW || digitalRead(lftBtnPin) == LOW ||digitalRead(rgtBtnPin) == LOW){
+        play = false;
+        break;
+      }
+    }  
+    if(!play) break;
+    strip.show();   // write all the pixels out
+    delay(wait);
   }
+}
+
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+uint8_t readButtonST7735(void) {
+  float a = analogRead(3);
+  
+  a *= 5.0;
+  a /= 1024.0;
+  
+  Serial.print("Button read analog = ");
+  Serial.println(a);
+  if (a < 0.2) return BUTTON_DOWN;
+  if (a < 1.0) return BUTTON_RIGHT;
+  if (a < 1.5) return BUTTON_SELECT;
+  if (a < 2.0) return BUTTON_UP;
+  if (a < 3.2) return BUTTON_LEFT;
+  else return BUTTON_NONE;
 }
 
 void NewGame(void)
@@ -116,8 +243,8 @@ void NewGame(void)
   SnakeHeadID = 1;
   SnakeItemPosX[1] = 1;
   SnakeItemPosY[1] = 1;
-  movingDirection = SNAKE_RIGHT;
-  snakeDirection = SNAKE_RIGHT;
+  movingDirection = SNAKE_DOWN;
+  snakeDirection = SNAKE_DOWN;
 
   for (y = 1; y <= BaneGridYmax; y++)
   {
@@ -126,7 +253,7 @@ void NewGame(void)
       Playfield[x][y] = BLANK;
     }
   }
-		
+
   AddSnakeItem = 0;
   AppleCount = 0;
   placeRandomApple();
@@ -134,81 +261,6 @@ void NewGame(void)
   GameRunning = true; 
   render(); 
   timeBefore = millis();
-}
-
-void setXYpixel(char x, char y, unsigned char R, unsigned char G, unsigned char B)
-{ 
-  if ((y % 2) > 0)
-    strip.setPixelColor((((6-y+1) * 7) - (6-x+1)), R, G, B);
-  else
-    strip.setPixelColor((((6-y) * 7) + (6-x)), R, G, B);
-}
-
-void placeApple(byte x, byte y) {
-  if (x > 0 && y > 0 && x <= BaneGridXmax && y <= BaneGridYmax) {
-    Playfield[x][y] = APPLE;
-    ApplePosX = x;
-    ApplePosY = y;
-    AppleMoveCountDown = random(APPLE_COUNTDOWN_STEPS_MIN, APPLE_COUNTDOWN_STEPS_MAX);
-			
-    AppleCount++;
-  }
-}
-
-void removeApple(void) {
-  Playfield[ApplePosX][ApplePosY] = BLANK;
-  AppleCount = 0; 
-}
-
-void render(void) { // Render de forskellige snake Items
-  byte i, x, y;
-  
-  for (i=1; i <= oldSnakeItems; i++) {
-    if (oldSnakeItemPosX[i] > 0 && oldSnakeItemPosY[i] > 0 && oldSnakeItemPosX[i] <= BaneGridXmax && oldSnakeItemPosY[i] <= BaneGridYmax) {
-      Playfield[oldSnakeItemPosX[i]][oldSnakeItemPosY[i]] = BLANK;
-    }
-  }
-		
-  for (i=1; i <= SnakeItems; i++) {
-    if (SnakeItemPosX[i] > 0 && SnakeItemPosY[i] > 0 && SnakeItemPosX[i] <= BaneGridXmax && SnakeItemPosY[i] <= BaneGridYmax) {			
-      Playfield[SnakeItemPosX[i]][SnakeItemPosY[i]] = SNAKE;
-      oldSnakeItemPosX[i] = SnakeItemPosX[i];
-      oldSnakeItemPosY[i] = SnakeItemPosY[i];			
-    }
-  }
-  oldSnakeItems = SnakeItems;
-  
-  if (AppleCount > 0 && AppleMoveCountDown == 0) {
-    removeApple();
-    placeRandomApple();
-  } else if (AppleCount > 0) {
-    AppleMoveCountDown--;
-  }
-  
-  for (y = 1; y <= BaneGridYmax; y++)
-  {
-    for (x = 1; x <= BaneGridXmax; x++)
-    {
-      switch (Playfield[x][y]) {
-        case BLANK:       
-          setXYpixel((x-1), (y-1), 0, 0, 0);
-          break;       
-        case SNAKE:
-          if (SnakeItemPosX[SnakeHeadID] == x && SnakeItemPosY[SnakeHeadID] == y)
-            setXYpixel((x-1), (y-1), 255, 255, 0); // Yellow snake head
-          else
-            setXYpixel((x-1), (y-1), 0, 255, 0); // Green snake body
-          break;  
-        case APPLE:
-          setXYpixel((x-1), (y-1), 255, 0, 0);
-          break;   
-        default:   
-          setXYpixel((x-1), (y-1), 0, 0, 0);
-          break; 
-      }          
-    }
-  }
-  strip.show();
 }
 
 void moveSnake(void) {
@@ -256,11 +308,11 @@ void moveSnake(void) {
 	SnakeItemPosY[SnakeHeadID] -= 1;
 	break;
     }	
-			
+
     SnakeItems++;			
     AddSnakeItem--;
   }
-		
+
     // Befinder vi os inden for banen?						
     if (SnakeItemPosX[SnakeHeadID] > 0 && SnakeItemPosX[SnakeHeadID] <= BaneGridXmax && SnakeItemPosY[SnakeHeadID] > 0 && SnakeItemPosY[SnakeHeadID] <= BaneGridYmax) {
       if (Playfield[SnakeItemPosX[SnakeHeadID]][SnakeItemPosY[SnakeHeadID]] != SNAKE) { // Er hovedets position på et blankt eller æble felt?
@@ -280,8 +332,7 @@ void moveSnake(void) {
       GameOver();
     }		
   }
-
-
+  
 void placeRandomApple(void) {
   byte x, y;
   x = random(1, BaneGridXmax);
@@ -292,6 +343,85 @@ void placeRandomApple(void) {
   }
   placeApple(x, y);
 }
+
+
+void placeApple(byte x, byte y) {
+  if (x > 0 && y > 0 && x <= BaneGridXmax && y <= BaneGridYmax) {
+    Playfield[x][y] = APPLE;
+    ApplePosX = x;
+    ApplePosY = y;
+    AppleMoveCountDown = random(APPLE_COUNTDOWN_STEPS_MIN, APPLE_COUNTDOWN_STEPS_MAX);
+
+    AppleCount++;
+  }
+}
+
+void removeApple(void) {
+  Playfield[ApplePosX][ApplePosY] = BLANK;
+  AppleCount = 0; 
+}
+
+void setXYpixel(char x, char y, unsigned char R, unsigned char G, unsigned char B)
+{ 
+  /*
+  if ((y % 2) > 0)
+    strip.setPixelColor(x, y, R, G, B);
+  else
+  */
+    strip.setPixelColor(x, y, R, G, B);
+}
+
+void render(void) { // Render de forskellige snake Items
+  byte i, x, y;
+  
+  for (i=1; i <= oldSnakeItems; i++) {
+    if (oldSnakeItemPosX[i] > 0 && oldSnakeItemPosY[i] > 0 && oldSnakeItemPosX[i] <= BaneGridXmax && oldSnakeItemPosY[i] <= BaneGridYmax) {
+      Playfield[oldSnakeItemPosX[i]][oldSnakeItemPosY[i]] = BLANK;
+    }
+  }
+
+  for (i=1; i <= SnakeItems; i++) {
+    if (SnakeItemPosX[i] > 0 && SnakeItemPosY[i] > 0 && SnakeItemPosX[i] <= BaneGridXmax && SnakeItemPosY[i] <= BaneGridYmax) {			
+      Playfield[SnakeItemPosX[i]][SnakeItemPosY[i]] = SNAKE;
+      oldSnakeItemPosX[i] = SnakeItemPosX[i];
+      oldSnakeItemPosY[i] = SnakeItemPosY[i];			
+    }
+  }
+  oldSnakeItems = SnakeItems;
+  
+  if (AppleCount > 0 && AppleMoveCountDown == 0) {
+    removeApple();
+    placeRandomApple();
+  } else if (AppleCount > 0) {
+    AppleMoveCountDown--;
+  }
+  
+  for (y = 1; y <= BaneGridYmax; y++)
+  {
+    for (x = 1; x <= BaneGridXmax; x++)
+    {
+      switch (Playfield[x][y]) {
+        case BLANK:       
+          setXYpixel((x-1), (y-1), 0, 0, 0);
+          break;       
+        case SNAKE:
+          if (SnakeItemPosX[SnakeHeadID] == x && SnakeItemPosY[SnakeHeadID] == y)
+            setXYpixel((x-1), (y-1), 255, 255, 0); // Yellow snake head
+          else
+            setXYpixel((x-1), (y-1), 0, 255, 0); // Green snake body
+          break;  
+        case APPLE:
+          setXYpixel((x-1), (y-1), 255, 0, 0);
+          break;   
+        default:   
+          setXYpixel((x-1), (y-1), 0, 0, 0);
+          break; 
+      }          
+    }
+  }
+  strip.show();
+}
+
 
 void GameOver(void) {
   if (SnakeItemPosX[SnakeHeadID] == 0) SnakeItemPosX[SnakeHeadID]++;
